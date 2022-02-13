2dbe07f0d7f50673a6002c67449a48e4c699e72b
ENOSISBD\ashraful <ashraful.fuad@enosisbd.com>
2022-02-13 12:26:45 +0600

    user-profile-model-added

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+USER_CHOICES = (
+    ('doctor','DOCTOR'),
+    ('patient','PATIENT'),
+    ('admin','ADMIN'),
+)
+
+class Profile(models.Model):
+    name = models.CharField(max_length=255)
+    user_type = models.CharField(max_length=50, choices=USER_CHOICES)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    
+    def __str__(self):
+        return self.name
